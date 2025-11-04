65f9acb7fd766b1a8752e4a6c13f9288a83be47b
hquino1 <143025124+hquino1@users.noreply.github.com>
2025-11-04 11:30:49 -0600

    added auth middleware for auth token (#13)

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import authRoutes from "./app/routes/auth.route.js";
 const app = express();
 app.use(cors());
 dotenv.config();
 const port = process.env.PORT || 3000;
+
+app.use("/auth", authRoutes);
 
 app.get("/", (req: any, res: any) => {
   res.send("Hello World");
