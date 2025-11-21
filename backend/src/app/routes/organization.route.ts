1fc5d02a21fa192da2ae8a16edc000ab60cfd8b2
Hector Quinones <quinonhector1@gmail.com>
2025-11-21 13:18:54 -0600

    added endponit for a normal user to join org

--- backend/src/app/routes/organization.route.ts
+++ backend/src/app/routes/organization.route.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { requireAuth } from "../middleware/auth.middleware.js";
-import { createOrganization, getOrganizationsById } from "../controllers/organization.controller.js";
+import { createOrganization, getOrganizationsById, joinOrganization } from "../controllers/organization.controller.js";
 
 const router = express.Router();
 
 router.post('/', requireAuth, createOrganization);
+router.post('/member', requireAuth, joinOrganization);
 router.get('/', requireAuth, getOrganizationsById);
 
 export default router;
